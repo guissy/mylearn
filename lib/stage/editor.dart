fde2b2f01018d5f3bc03229beb3a0e9096e547ba
桂子 <guissy@qq.com>
2019-09-07 00:36:33 +0800

    code bg style

--- lib/stage/editor.dart
+++ lib/stage/editor.dart
@@ -8,8 +8,52 @@ class Editor extends StatefulWidget {
 class _EditorState extends State<Editor> {
   @override
   Widget build(BuildContext context) {
+    List<String> code = 'for i in range(0, 10): \n   pass'.split('\n');
     return Container(
-      child: Text('for i in range(0, 10): pass', style: TextStyle(fontSize: 22.0),),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisSize: MainAxisSize.max,
+        children: <Widget>[
+          Container(
+            width: 90.0,
+            height: MediaQuery.of(context).size.height - 90.0,
+            color: Color(0xfffffbef),
+            child: ListView.builder(
+                itemCount: code.length,
+                itemBuilder: (content, i) => Container(
+                      height: 50.0,
+                      alignment: Alignment.centerRight,
+                      padding: EdgeInsets.symmetric(
+                          vertical: 10.0, horizontal: 20.0),
+                      child: Text(
+                        (i + 1).toString(),
+                        style:
+                            TextStyle(color: Color(0xff975e17), fontSize: 24.0),
+                      ),
+                    )),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              margin: EdgeInsets.only(top: 5.0),
+              color: Color(0xfffffff5),
+              child: ListView.builder(
+                  itemCount: code.length,
+                  itemBuilder: (content, i) => Container(
+                        height: 50.0,
+                        alignment: Alignment.centerLeft,
+                        padding: EdgeInsets.symmetric(
+                            vertical: 10.0, horizontal: 20.0),
+                        child: Text(
+                          code[i],
+                          style: TextStyle(
+                              color: Color(0xff4ca9e9), fontSize: 24.0),
+                        ),
+                      )),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
